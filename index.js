cd011bad11afd4969dc5c4fe60a8e963e930ae97
Wolferine189 <92498047+Wolferine189@users.noreply.github.com>
2022-03-01 17:10:32 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+function show(){
+    var x=document.getElementById("times");
+    var y=document.getElementById("bars");
+    var z=document.getElementById("uls");
+   
+    x.style.display="block";
+    y.style.display="none";
+    z.style.display="block";
+   
+
+}
+function hide(){
+    var x=document.getElementById("times");
+    var y=document.getElementById("bars");
+    var z=document.getElementById("uls");
+  
+    x.style.display="none";
+    y.style.display="block";
+    z.style.display="none";
+   
+}
